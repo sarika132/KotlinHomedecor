bf7627ccbc159f8a8bcb38d4330e1d675051b73a
Sarika Pandey <sarikapandey603@gmail.com>
2025-07-28 08:39:21 +0545

    Update

--- app/src/main/java/com/example/myapplication/view/ListViewActivity.kt
+++ app/src/main/java/com/example/myapplication/view/ListViewActivity.kt
@@ -44,165 +44,281 @@ class ListViewActivity : ComponentActivity() {
 
 @Composable
 fun ListViewBody(){
-    val images = listOf(
-        R.drawable.aaa,
-        R.drawable.img,
-        R.drawable.cccc,
-        R.drawable.dddd,
-        R.drawable.eeeee,
-        R.drawable.img,
-        R.drawable.imgs
-
-    )
-    val name = listOf(
-        "aaa",
-        "bbb",
-        "cccc",
-        "dddd",
-        "eeeee",
-        "img",
-        "imgs"
-    )
-    Scaffold { padding ->
-
-        LazyColumn(modifier = Modifier
-            .padding(padding)
-            .fillMaxSize(),
-
-
+    Column(modifier=
+        Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .fillMaxSize().background(color = Color.Cyan)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(start = 19.dp, top = 19.dp, end = 19.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        )
+        {
+            Image(
+                painter = painterResource(R.drawable.profile),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(50.dp)
+                    .clip(shape = RoundedCornerShape(80.dp))
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 17.dp),
+        ) {
+            Text(
+                text = "Card", style =
+                    TextStyle(
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold, color = Color.White,
+                        fontStyle = FontStyle.Normal
+                    )
+            )
+            Text(
+                "Simple and easy to use app", style = TextStyle(
+                    fontWeight = FontWeight.SemiBold, color = Color.White
+                )
+            )
+        }
+        Row (
+            modifier = Modifier
+                .padding(start = 17.dp, top = 40.dp, end = 2.dp, bottom = 2.dp),
+        ) {
+            //First Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
             ) {
-            item {
-
-                LazyVerticalGrid(modifier = Modifier
-                    .height(320.dp)
-                    .fillMaxWidth(),
-                    columns = GridCells.Fixed(3)
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    items(images.size){ index: Int ->
-                        Column  (
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ){
-                            Image(
-                                painterResource(images[index]),
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier.size(80.dp).clip(shape = CircleShape)
-                            )
-                            Text(name[index])
-                        }
-                    }
+                    Image(
+                        painter = painterResource(R.drawable.text),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Text", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
                 }
-                LazyRow(modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    items(images.size){index ->
-                        Column  (
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-
-
-                        ) {
-                            Image(
-                                painterResource(images[index]),
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier.size(80.dp).clip(shape = CircleShape)
-                            )
-
-                            Text(name[index])
-                        }
-
-                    }
-                }
-
-                LazyRow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-
-                ) {
-                    item {
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.Gray)
-                                .padding(10.dp)
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.DarkGray)
-                                .padding(10.dp)
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.LightGray)
-                                .padding(10.dp)
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.Blue)
-                                .padding(10.dp)
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.Yellow)
-                                .padding(10.dp)
-                        )
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .background(color = Color.Red)
-                                .padding(10.dp)
-                        )
-                    }
-                }
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Red)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Yellow)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Blue)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Magenta)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Green)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth().padding(20.dp)
-                        .background(Color.Black)
-                )
-
-
 
             }
-        }
-    }
+            Spacer(modifier = Modifier.width(35.dp))
 
+            // Second Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.addresh),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Addresh", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
+                }
+
+            }
+
+        }
+        Row (
+            modifier = Modifier
+                .padding(start = 17.dp, top = 25.dp, end = 2.dp, bottom = 2.dp),
+        ) {
+            //Third Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.character),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Character", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
+                }
+
+            }
+            Spacer(modifier = Modifier.width(35.dp))
+
+            // Forth Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.bankcard),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Bank Card", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
+                }
+
+            }
+
+        }
+        Row (
+            modifier = Modifier
+                .padding(start = 17.dp, top = 25.dp, end = 2.dp, bottom = 2.dp),
+        ) {
+            //Fifth Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.password),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Password", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
+                }
+
+            }
+            Spacer(modifier = Modifier.width(35.dp))
+
+            // Sixth Card
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(color = Color.White)
+                    .width(160.dp)
+                    .height(160.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.logistics),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(60.dp)
+                    )
+                    Text(
+                        text = "Logistics", style = TextStyle(
+                            fontWeight = FontWeight.SemiBold, fontSize = 20.sp
+                        )
+                    )
+                    Text(text = "@ Click to Enter", color = Color.Gray)
+                }
+            }
+        }
+
+        Row (
+            modifier = Modifier
+                .padding(start = 17.dp, top = 25.dp, end = 2.dp, bottom = 2.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = Color.White)
+                    .width(360.dp)
+                    .height(70.dp)
+
+            ) {
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 20.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(30.dp)
+                            .width(30.dp)
+                    )
+                    Text(text = " Settings",style = TextStyle(
+                        fontWeight = FontWeight.SemiBold, fontSize = 20.sp))
+                }
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
